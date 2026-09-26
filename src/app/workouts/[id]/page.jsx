8558fd60c5eac31f113ecdc3f94f045ad23743a7
fidahaque21 <fidahaque1215@gmail.com>
2026-09-26 22:52:48 +0600

    clean  & Fresh code

--- src/app/workouts/[id]/page.jsx
+++ src/app/workouts/[id]/page.jsx
@@ -1,23 +1,3 @@
-// import WorkoutDetails from "@/app/components/workout/WorkoutDetails";
-
-// const WorkoutPage = async ({ params }) => {
-//   const { id } = await params;
-
-//   //  API call
-//   const res = await fetch(
-//     `https://api.abcz.workers.dev/api/fitlog/${id}`
-//   );
-
-//   const workout = await res.json();
-
-//   return (
-//     <div>
-//       <WorkoutDetails workout={workout} />
-//     </div>
-//   );
-// };
-// export default WorkoutPage;
-
 import WorkoutDetails from "@/app/components/workout/WorkoutDetails";
 
 const WorkoutPage = async ({ params }) => {
